67a261b6f07973e5b9b8ff6e8a583793a93207de
xhnv3gts <gunon2hayo.3zgy@gmail.com>
2025-08-14 05:39:51 +0900

    書の一覧をjsonから取得に変更

--- index.js
+++ index.js
@@ -399,22 +399,10 @@ function getNormalizedText(text) {
     let recentlyNormalizedText = null;
 
     let words = null;
-    const bookNames = [
-        'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
-        'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel',
-        '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra',
-        'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs',
-        'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations',
-        'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos',
-        'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
-        'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
-        'Matthew', 'Mark', 'Luke', 'John', 'Acts',
-        'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians',
-        'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy',
-        '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
-        '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
-        'Jude', 'Revelation'
-    ];
+    let bookNames;
+    (async () => {
+        bookNames = await getData('./data/book-names.json');
+    })();
 
 
     document.getElementById('practice-reception').addEventListener('click', async () => {
@@ -581,14 +569,13 @@ document.getElementById('clear-canvas').addEventListener('click', () => {
 });
 
 //fetch
-async function getData() {
+async function getData(path) {
     try {
-        const response = await fetch('./data/test.json');
+        const response = await fetch(path);
         if (!response.ok) {
             throw new Error('HTTPエラー: ' + response.status);
         }
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (error) {
         return null;
