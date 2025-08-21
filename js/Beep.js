d8dbbc44dad1c9272b2efd97204aa8fa4824e181
xhnv3gts <gunon2hayo.3zgy@gmail.com>
2025-08-22 03:07:33 +0900

    Beepクラス変更

--- js/Beep.js
+++ js/Beep.js
@@ -1,19 +1,49 @@
 export default class Beep {
-    static #audioCtx;
-    static #timeoutId2;
-    static #frequency = 770;
-    static #gain;
+    static #isPlaying = false;
+    static #audioCtx = new AudioContext();
+    static #oscillatorNode;
+    static #waveform = 'sawtooth';
+    static #frequency = 900;
+    static #gainNode = new GainNode(this.#audioCtx, { gain: 0 });
+    static #volume = 0;
+    static #VOLUME_SCALE_FACTOR = 0.0006;
     static play(duration) {
+        if (this.#isPlaying) { return Promise.resolve(); }
+        this.#isPlaying = true;
         return new Promise(resolve => {
-            if (!this.#audioCtx) { this.#audioCtx = new AudioContext(); }
-            const oscillator = new OscillatorNode(this.#audioCtx, { type: 'sawtooth', frequency: this.#frequency });
-            oscillator.onended = () => resolve();
-            const gainNode = new GainNode(this.#audioCtx, { gain: this.#gain });
-            oscillator.connect(gainNode).connect(this.#audioCtx.destination);
-            oscillator.start();
-            oscillator.stop(this.#audioCtx.currentTime + duration / 1000);
+            const oscillatorNode = this.#oscillatorNode = new OscillatorNode(this.#audioCtx, { type: this.#waveform, frequency: this.#frequency });
+            oscillatorNode.onended = () => {
+                this.#isPlaying = false;
+                resolve();
+            };
+            oscillatorNode.connect(this.#gainNode).connect(this.#audioCtx.destination);
+            oscillatorNode.start();
+            oscillatorNode.stop(this.#audioCtx.currentTime + duration / 1000);
         });
     }
+    static cancel() {
+        if (this.#isPlaying) { this.#oscillatorNode.stop(); }
+    }
+    static setWaveform(waveform) {
+        this.#waveform = waveform;
+        if (this.#isPlaying) { this.#oscillatorNode.type = waveform; }
+    }
+    static setFrequency(frequency) {
+        this.#frequency = frequency;
+        if (this.#isPlaying) { this.#oscillatorNode.frequency.value = frequency; }
+    }
+    static setVolume(volume) {
+        this.#volume = (!Number.isInteger(volume) || volume < 0) ? 0 : (volume > 100) ? 100 : volume;
+        this.#gainNode.gain.value = this.#volume * this.#VOLUME_SCALE_FACTOR;
+    }
+    static getSettings() {
+        return { waveform: this.#waveform, frequency: this.#frequency, volume: this.#volume };
+    }
+}
+
+
+
+    // static #timeoutId2;
     // static async play(duration) {
     //     try {
     //         // if (this.#audioCtx && this.#audioCtx.state === 'suspended') { this.#audioCtx.close(); }
@@ -59,7 +89,3 @@ export default class Beep {
     //         // document.getElementById('log').append('[innercatch]');
     //     }
     // }
-    static setVolume(volume) {
-        this.#gain = volume * 0.0006;
-    }
-}
