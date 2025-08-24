9fa92e38a39d003958843bb7e5704917d71d8772
xhnv3gts <gunon2hayo.3zgy@gmail.com>
2025-08-25 08:41:07 +0900

    Beepクラス iOS,Safariで正常に再生できるように修正

--- js/Beep.js
+++ js/Beep.js
@@ -1,19 +1,29 @@
 export default class Beep {
     static #isPlaying = false;
-    static #audioCtx = new AudioContext();
+    static #resetAudioCtxTimeoutId;
+    static #RESET_AUDIO_CTX_TIMEOUT = 5000;
+    static #audioCtx;
     static #oscillatorNode;
     static #waveform = 'sawtooth';
     static #frequency = 900;
-    static #gainNode = new GainNode(this.#audioCtx, { gain: 0 });
+    static #gainNode;
+    static #gain = 0;
     static #volume = 0;
     static #VOLUME_SCALE_FACTOR = 0.0006;
     static play(duration) {
         if (this.#isPlaying) { return Promise.resolve(); }
         this.#isPlaying = true;
+        clearTimeout(this.#resetAudioCtxTimeoutId);
+
+        if (!this.#audioCtx) { this.#initializeAudioCtx(); }
         return new Promise(resolve => {
             const oscillatorNode = this.#oscillatorNode = new OscillatorNode(this.#audioCtx, { type: this.#waveform, frequency: this.#frequency });
             oscillatorNode.onended = () => {
                 this.#isPlaying = false;
+                this.#resetAudioCtxTimeoutId = setTimeout(async () => {
+                    await this.#audioCtx.close();
+                    this.#initializeAudioCtx();
+                }, this.#RESET_AUDIO_CTX_TIMEOUT);
                 resolve();
             };
             oscillatorNode.connect(this.#gainNode).connect(this.#audioCtx.destination);
@@ -34,58 +44,22 @@ export default class Beep {
     }
     static setVolume(volume) {
         this.#volume = (!Number.isInteger(volume) || volume < 0) ? 0 : (volume > 100) ? 100 : volume;
-        this.#gainNode.gain.value = this.#volume * this.#VOLUME_SCALE_FACTOR;
+        this.#gain = this.#volume * this.#VOLUME_SCALE_FACTOR;
+        if (this.#gainNode) { this.#gainNode.gain.value = this.#gain; }
     }
     static getSettings() {
         return { waveform: this.#waveform, frequency: this.#frequency, volume: this.#volume };
     }
+    static #initializeAudioCtx() {
+        this.#audioCtx = new AudioContext();
+        this.#audioCtx.onstatechange = async e => {
+            if (e.target.state === 'interrupted') {
+                this.#gainNode.gain.value = 0;
+                await e.target.resume(); //再開しないとstopが効かない
+                this.#oscillatorNode.stop();
+                this.#gainNode.gain.value = this.#gain;
+            }
+        };
+        this.#gainNode = new GainNode(this.#audioCtx, { gain: this.#gain });
+    }
 }
-
-
-
-    // static #timeoutId2;
-    // static async play(duration) {
-    //     try {
-    //         // if (this.#audioCtx && this.#audioCtx.state === 'suspended') { this.#audioCtx.close(); }
-    //         // if (!this.#audioCtx) { this.#audioCtx = new AudioContext(); }
-    //         // this.#audioCtx.close();
-    //         // this.#audioCtx = new AudioContext();
-    //         clearTimeout(this.#timeoutId2);
-    //         let audioCtx = new AudioContext();
-    //         // document.getElementById('log').append(this.#audioCtx.state[0]);
-    //         // document.getElementById('log').append(1);
-    //         // if (this.#audioCtx.state === 'suspended') {
-    //         //     document.getElementById('log').append(2);
-    //         //     await this.#audioCtx.resume();
-    //         // }
-    //         // document.getElementById('log').append(this.#audioCtx.state[0]);
-    //         // document.getElementById('log').append(3);
-    //         return new Promise(resolve => {
-    //             // document.getElementById('log').append(4);
-    //             const oscillator = new OscillatorNode(audioCtx, { type: 'sawtooth', frequency: this.#frequency });
-    //             const timeoutId = setTimeout(async () => {
-    //                 document.getElementById('log').append('[timeout-resolve]');
-    //                 await audioCtx.close();
-    //                 audioCtx = null;
-    //                 resolve();
-    //             }, duration + 100);
-    //             oscillator.onended = () => {
-    //                 // document.getElementById('log').append('[onended]');
-    //                 this.#timeoutId2 = setTimeout(async () => {
-    //                     await audioCtx.close();
-    //                     audioCtx = null;
-    //                 }, 1000);
-    //                 clearTimeout(timeoutId);
-    //                 resolve();
-    //             }
-    //             const gainNode = new GainNode(audioCtx, { gain: this.#gain });
-    //             oscillator.connect(gainNode).connect(audioCtx.destination);
-    //             oscillator.start();
-    //             oscillator.stop(audioCtx.currentTime + duration / 1000);
-    //             // document.getElementById('log').append(5);
-                
-    //         });
-    //     } catch {
-    //         // document.getElementById('log').append('[innercatch]');
-    //     }
-    // }
