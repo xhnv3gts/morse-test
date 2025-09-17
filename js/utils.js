d22b8afedbf8f43740e0dad6ee886f4683c7c9ce
xhnv3gts <gunon2hayo.3zgy@gmail.com>
2025-09-18 01:14:51 +0900

    utils.js 関数追加

--- js/utils.js
+++ js/utils.js
@@ -9,6 +9,34 @@ export async function getData(url) {
         return null;
     }
 }
+export async function getSettings(path) {
+    const settings = await getData(path);
+    const commonSettings = settings.common;
+    const filename = window.location.pathname.split('/').at(-1) || 'index.html';
+    const localSettings = settings[filename];
+    return deepMerge(commonSettings, localSettings);
+}
+function deepMerge(target, source) {
+    if (source === undefined) {
+        return target;
+    }
+
+    if (typeof source === 'object' && source !== null && !Array.isArray(source)) {
+        if (typeof target !== 'object' || target === null || Array.isArray(target)) {
+            target = {};
+        }
+        for (const key of Object.keys(source)) {
+            target[key] = deepMerge(target[key], source[key]);
+        }
+        return target;
+    }
+
+    if (Array.isArray(source)) {
+        return [...source];
+    }
+
+    return source;
+}
 
 export function getRandomItem(array) {
     const index = getRandomIndex(array);
@@ -23,4 +51,18 @@ export function getRandomSubarray(array, range) {
     const start = Math.floor(Math.random() * (array.length - range + 1));
     const end = start + range;
     return array.slice(start, end);
+}
+
+export function registerShortcutKey({ modifierKey, key, keyText, buttonId }) {
+    const button = document.getElementById(buttonId);
+    const modifierKeyText = { 'ctrlKey': 'Ctrl' }[modifierKey];
+    keyText ??= key.toUpperCase();
+    button.textContent += `[${modifierKeyText ? `${modifierKeyText} + ` : ''}${keyText}]`;
+    const isModifierKeyDown = modifierKey ? e => e[modifierKey] : () => true;
+    window.addEventListener('keydown', e => {
+        if (e.repeat || !isModifierKeyDown(e) || e.key !== key) { return; }
+        e.preventDefault();
+        button.dispatchEvent(new Event('click'));
+        console.log('a');
+    });
 }
